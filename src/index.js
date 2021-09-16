a720aeffe420e61ed3caf6234c4d7026efcd93d8
kaye garcia <kayemgarcia@gmail.com>
2021-09-16 10:40:59 -0500

    Done.

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//DOM Render functions
+function renderOnePup(pup){
+    //Build pup
+    let newSpan = document.createElement('span');
+    document.getElementById('dog-bar').appendChild(newSpan);
+    let textBox = document.createElement('input');
+    textBox.innerText = 'Mr. Bonkers'
+    console.log(textBox)
+
+    
+
+
+}
+
+
+
+
+//Fetch Requests
+//Get Fetch for all pup resources
+function getAllPups(){
+    fetch('http://localhost:3000/pups')
+    .then(response => response.json())
+    .then(pupData => pupData.forEach(pup => renderOnePup(pup)))
+}
+
+
+// Initial Render
+// Get Data and Render our Animals to the DOM
+
+function initialize(){
+    //pupData.forEach(pup => renderOnePup(pup))
+    getAllPups()
+    //console.log('after get All Pups')
+}
+
+initialize()
